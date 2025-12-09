db89d46f81c5463d41f5b39fbef0cae7162a4930
unknown <fahrezananda48@gmail.com>
2025-12-09 22:55:30 +0700

    [nanda] online

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies('*');
         $middleware->redirectGuestsTo('/login');
         $middleware->redirectUsersTo(function () {
             $role = request()->user()->role;
